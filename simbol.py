f5c85af0c58e5d5d1a9f122b525ae05ee1e2097e
EllyIMA <wiz33@yandex.ru>
2022-03-27 18:20:33 +0300

    рабочий вариантик улучшеный

--- simbol.py
+++ simbol.py
@@ -1,24 +1,28 @@
+def draw(massive, y, x, ):
+    dam = 0
+    for q in range(y):
 
-def draw(massive,y):
-     for q in range(y):
+        for g in massive[q]:
+            dam += 1
 
-          for g in massive[q]:
-               print(g[0], end='')
-
-
-
-def creator(x,y):
-     massive = []
-     for _ in range(y):
-          massive.append([' ' for _ in range(x)])
-     return massive
+            if dam == x:
+               dam = 0
+               print(g[0])
+            else:
+                print(g[0], end='')
 
 
-def coordination(symbol, x,y,x1,y1):
-     massive = creator(x1,y1)
-     massive[y][x] = symbol
-     draw(massive,y1)
+def creator(x, y):
+    massive = []
+    for _ in range(y):
+        massive.append(['x' for _ in range(x)])
+    return massive
 
-coordination('s',10,45,100,100)
+
+def coordination(symbol, x, y, x1, y1):
+    massive = creator(x1, y1)
+    massive[y][x] = symbol
+    draw(massive, y1, x1)
 
 
+coordination('s', 3, 5, 10, 10)
